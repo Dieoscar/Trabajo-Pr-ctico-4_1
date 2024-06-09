69f3dab2ecb59cab5f6452121749317da6aabafb
Dieoscar <dkloster79@yahoo.com.ar>
2024-06-09 16:17:41 -0300

    Programa Funcionando

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,145 +22,292 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
-        DataGridView1 = New DataGridView()
-        DataGridView2 = New DataGridView()
+        cmdEjecutar = New Button()
+        dtvMostrarDatos = New DataGridView()
+        dtvIngresarDatos = New DataGridView()
         Label1 = New Label()
+        Label4 = New Label()
+        dtvMostrarVector = New DataGridView()
         Label2 = New Label()
         Label3 = New Label()
-        Label4 = New Label()
+        Label7 = New Label()
         Label5 = New Label()
         Label6 = New Label()
-        DataGridView3 = New DataGridView()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
-        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
-        CType(DataGridView3, ComponentModel.ISupportInitialize).BeginInit()
+        Label8 = New Label()
+        txtNumeroN = New TextBox()
+        txtMayor = New TextBox()
+        txtUbicacionMayor = New TextBox()
+        lstPares = New ListBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        lstImpares = New ListBox()
+        lstMultiplos = New ListBox()
+        CType(dtvMostrarDatos, ComponentModel.ISupportInitialize).BeginInit()
+        CType(dtvIngresarDatos, ComponentModel.ISupportInitialize).BeginInit()
+        CType(dtvMostrarVector, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' Button1
+        ' cmdEjecutar
         ' 
-        Button1.Location = New Point(583, 396)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(185, 29)
-        Button1.TabIndex = 0
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        cmdEjecutar.BackColor = Color.YellowGreen
+        cmdEjecutar.Font = New Font("Rockwell", 16F, FontStyle.Bold)
+        cmdEjecutar.Location = New Point(1071, 646)
+        cmdEjecutar.Name = "cmdEjecutar"
+        cmdEjecutar.Size = New Size(185, 52)
+        cmdEjecutar.TabIndex = 0
+        cmdEjecutar.Text = "Ejecutar"
+        cmdEjecutar.UseVisualStyleBackColor = False
         ' 
-        ' DataGridView1
+        ' dtvMostrarDatos
         ' 
-        DataGridView1.BackgroundColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(12, 62)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.RowHeadersWidth = 51
-        DataGridView1.Size = New Size(300, 52)
-        DataGridView1.TabIndex = 1
+        dtvMostrarDatos.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells
+        dtvMostrarDatos.BackgroundColor = SystemColors.GradientInactiveCaption
+        dtvMostrarDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvMostrarDatos.Location = New Point(728, 60)
+        dtvMostrarDatos.Name = "dtvMostrarDatos"
+        dtvMostrarDatos.RowHeadersWidth = 51
+        dtvMostrarDatos.Size = New Size(528, 54)
+        dtvMostrarDatos.TabIndex = 1
         ' 
-        ' DataGridView2
+        ' dtvIngresarDatos
         ' 
-        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView2.Location = New Point(361, 62)
-        DataGridView2.Name = "DataGridView2"
-        DataGridView2.RowHeadersWidth = 51
-        DataGridView2.Size = New Size(300, 52)
-        DataGridView2.TabIndex = 2
+        dtvIngresarDatos.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells
+        dtvIngresarDatos.BackgroundColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        dtvIngresarDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvIngresarDatos.Location = New Point(22, 60)
+        dtvIngresarDatos.Name = "dtvIngresarDatos"
+        dtvIngresarDatos.RowHeadersWidth = 51
+        dtvIngresarDatos.Size = New Size(528, 54)
+        dtvIngresarDatos.TabIndex = 2
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
+        Label1.Font = New Font("Verdana", 12F, FontStyle.Bold)
         Label1.Location = New Point(20, 11)
         Label1.Name = "Label1"
-        Label1.Size = New Size(53, 20)
+        Label1.Size = New Size(190, 25)
         Label1.TabIndex = 3
-        Label1.Text = "Label1"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(361, 11)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(53, 20)
-        Label2.TabIndex = 4
-        Label2.Text = "Label2"
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(20, 266)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(53, 20)
-        Label3.TabIndex = 5
-        Label3.Text = "Label3"
+        Label1.Text = "Carga de Vector"
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
         Label4.Location = New Point(20, 316)
         Label4.Name = "Label4"
-        Label4.Size = New Size(53, 20)
+        Label4.Size = New Size(0, 20)
         Label4.TabIndex = 6
-        Label4.Text = "Label4"
+        ' 
+        ' dtvMostrarVector
+        ' 
+        dtvMostrarVector.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells
+        dtvMostrarVector.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dtvMostrarVector.Location = New Point(20, 646)
+        dtvMostrarVector.Name = "dtvMostrarVector"
+        dtvMostrarVector.RowHeadersWidth = 51
+        dtvMostrarVector.Size = New Size(528, 54)
+        dtvMostrarVector.TabIndex = 9
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label2.Location = New Point(904, 11)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(191, 25)
+        Label2.TabIndex = 10
+        Label2.Text = " Vector Cargado"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label3.Location = New Point(22, 130)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(196, 25)
+        Label3.TabIndex = 11
+        Label3.Text = "Carga Número N"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label7.Location = New Point(12, 352)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(298, 25)
+        Label7.TabIndex = 12
+        Label7.Text = "Número Mayor del Vector"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(259, 316)
+        Label5.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label5.Location = New Point(12, 475)
         Label5.Name = "Label5"
-        Label5.Size = New Size(53, 20)
-        Label5.TabIndex = 7
-        Label5.Text = "Label5"
+        Label5.Size = New Size(341, 25)
+        Label5.TabIndex = 13
+        Label5.Text = " Ubicación del Número Mayor"
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(20, 369)
+        Label6.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label6.Location = New Point(444, 130)
         Label6.Name = "Label6"
-        Label6.Size = New Size(53, 20)
-        Label6.TabIndex = 8
-        Label6.Text = "Label6"
+        Label6.Size = New Size(386, 25)
+        Label6.TabIndex = 14
+        Label6.Text = " Multiplos del N° ""N"" en el vector"
         ' 
-        ' DataGridView3
+        ' Label8
         ' 
-        DataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView3.Location = New Point(20, 405)
-        DataGridView3.Name = "DataGridView3"
-        DataGridView3.RowHeadersWidth = 51
-        DataGridView3.Size = New Size(300, 52)
-        DataGridView3.TabIndex = 9
+        Label8.AutoSize = True
+        Label8.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label8.Location = New Point(22, 593)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(197, 25)
+        Label8.TabIndex = 15
+        Label8.Text = "Vector Invertido"
+        ' 
+        ' txtNumeroN
+        ' 
+        txtNumeroN.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        txtNumeroN.Font = New Font("Verdana", 15F, FontStyle.Bold)
+        txtNumeroN.Location = New Point(22, 160)
+        txtNumeroN.Name = "txtNumeroN"
+        txtNumeroN.Size = New Size(89, 38)
+        txtNumeroN.TabIndex = 16
+        txtNumeroN.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtMayor
+        ' 
+        txtMayor.BackColor = SystemColors.GradientActiveCaption
+        txtMayor.Font = New Font("Verdana", 15F, FontStyle.Bold)
+        txtMayor.Location = New Point(12, 407)
+        txtMayor.Name = "txtMayor"
+        txtMayor.Size = New Size(89, 38)
+        txtMayor.TabIndex = 18
+        txtMayor.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtUbicacionMayor
+        ' 
+        txtUbicacionMayor.BackColor = SystemColors.GradientActiveCaption
+        txtUbicacionMayor.Font = New Font("Verdana", 15F, FontStyle.Bold)
+        txtUbicacionMayor.Location = New Point(12, 531)
+        txtUbicacionMayor.Name = "txtUbicacionMayor"
+        txtUbicacionMayor.Size = New Size(89, 38)
+        txtUbicacionMayor.TabIndex = 19
+        txtUbicacionMayor.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' lstPares
+        ' 
+        lstPares.BackColor = SystemColors.GradientActiveCaption
+        lstPares.Cursor = Cursors.No
+        lstPares.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        lstPares.FormattingEnabled = True
+        lstPares.ItemHeight = 25
+        lstPares.Location = New Point(456, 390)
+        lstPares.Name = "lstPares"
+        lstPares.Size = New Size(94, 179)
+        lstPares.TabIndex = 23
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label9.Location = New Point(456, 352)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(183, 25)
+        Label9.TabIndex = 24
+        Label9.Text = "Números Pares"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        Label10.Location = New Point(763, 352)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(214, 25)
+        Label10.TabIndex = 25
+        Label10.Text = "Números Impares"
+        ' 
+        ' lstImpares
+        ' 
+        lstImpares.BackColor = SystemColors.GradientActiveCaption
+        lstImpares.Cursor = Cursors.No
+        lstImpares.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        lstImpares.FormattingEnabled = True
+        lstImpares.ItemHeight = 25
+        lstImpares.Location = New Point(770, 390)
+        lstImpares.Name = "lstImpares"
+        lstImpares.Size = New Size(121, 179)
+        lstImpares.TabIndex = 26
+        ' 
+        ' lstMultiplos
+        ' 
+        lstMultiplos.BackColor = SystemColors.GradientActiveCaption
+        lstMultiplos.Cursor = Cursors.No
+        lstMultiplos.Font = New Font("Verdana", 12F, FontStyle.Bold)
+        lstMultiplos.FormattingEnabled = True
+        lstMultiplos.ItemHeight = 25
+        lstMultiplos.Location = New Point(456, 158)
+        lstMultiplos.Name = "lstMultiplos"
+        lstMultiplos.Size = New Size(114, 179)
+        lstMultiplos.TabIndex = 27
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 632)
-        Controls.Add(DataGridView3)
+        BackColor = SystemColors.InactiveCaption
+        ClientSize = New Size(1268, 712)
+        Controls.Add(lstMultiplos)
+        Controls.Add(lstImpares)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(lstPares)
+        Controls.Add(txtUbicacionMayor)
+        Controls.Add(txtMayor)
+        Controls.Add(txtNumeroN)
+        Controls.Add(Label8)
         Controls.Add(Label6)
         Controls.Add(Label5)
-        Controls.Add(Label4)
+        Controls.Add(Label7)
         Controls.Add(Label3)
         Controls.Add(Label2)
+        Controls.Add(dtvMostrarVector)
+        Controls.Add(Label4)
         Controls.Add(Label1)
-        Controls.Add(DataGridView2)
-        Controls.Add(DataGridView1)
-        Controls.Add(Button1)
+        Controls.Add(dtvIngresarDatos)
+        Controls.Add(dtvMostrarDatos)
+        Controls.Add(cmdEjecutar)
         Name = "Form1"
-        Text = "Form1"
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
-        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
-        CType(DataGridView3, ComponentModel.ISupportInitialize).EndInit()
+        Text = "Vectores"
+        CType(dtvMostrarDatos, ComponentModel.ISupportInitialize).EndInit()
+        CType(dtvIngresarDatos, ComponentModel.ISupportInitialize).EndInit()
+        CType(dtvMostrarVector, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents cmdEjecutar As Button
+    Friend WithEvents dtvMostrarDatos As DataGridView
+    Friend WithEvents dtvIngresarDatos As DataGridView
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents dtvMostrarVector As DataGridView
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents DataGridView3 As DataGridView
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtNumeroN As TextBox
+    Friend WithEvents txtMayor As TextBox
+    Friend WithEvents txtUbicacionMayor As TextBox
+    Friend WithEvents lstPares As ListBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents lstImpares As ListBox
+    Friend WithEvents lstMultiplos As ListBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,78 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtvMostrarDatos.ColumnCount = 10
+        dtvMostrarDatos.Rows.Add(4)
+        For i = 0 To 9
+            dtvMostrarDatos.Columns(i).Width = 45
+        Next
+        dtvIngresarDatos.ColumnCount = 10
+        dtvIngresarDatos.Rows.Add(4)
+        For i = 0 To 9
+            dtvIngresarDatos.Columns(i).Width = 45
+        Next
+        dtvMostrarVector.ColumnCount = 10
+        dtvMostrarVector.Rows.Add(4)
+        For i = 0 To 9
+            dtvMostrarVector.Columns(i).Width = 45
+
+        Next
+    End Sub
+
+
+
+
+    Private Sub cmdEjecutar_Click_1(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim VectorIngresar(0 To 9) As Integer
+        Dim vectorInvertido(0 To 9) As Integer
+        Dim Contador As Integer
+        Dim Mayor As Integer
+        Dim NumeroN As Integer
+        Dim Posicion As Integer
+        Dim RestoPar As Integer
+        Dim RestoN As Integer
+        Dim x As Integer
+        Dim c As Integer
+        Mayor = 0
+        For c = 0 To 9
+
+            VectorIngresar(c) = dtvIngresarDatos.Item(c, 0).Value
+
+        Next
+        NumeroN = txtNumeroN.Text
+        For c = 0 To 9
+            dtvMostrarDatos.Item(c, 0).Value = VectorIngresar(c)
+            RestoPar = VectorIngresar(c) Mod 2
+            If RestoPar = 0 Then
+                lstPares.Items.Add(VectorIngresar(c))
+            Else
+                lstImpares.Items.Add(VectorIngresar(c))
+            End If
+
+            RestoN = VectorIngresar(c) Mod NumeroN
+            If RestoN = 0 Then
+                lstMultiplos.Items.Add(VectorIngresar(c))
+            End If
+            If VectorIngresar(c) > Mayor Then
+                Mayor = VectorIngresar(c)
+                Posicion = c + 1
+
+            End If
+            txtMayor.Text = Mayor
+            txtUbicacionMayor.Text = Posicion
+
+
+        Next
+
+        For c = 0 To 9
+
+            vectorInvertido(c) = VectorIngresar(c)
+        Next
+        For c = 0 To 9
+            Dim d As Integer
+            Contador = 9 - c
+            dtvMostrarVector.Item(c, 0).Value = vectorInvertido(Contador)
+        Next
+    End Sub
+
 
 End Class
